89f1a285c220686805b19cd009c580a8c38b69b0
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-07-19 17:45:29 +0200

    the end

--- routes/tweet.js
+++ routes/tweet.js
@@ -9,7 +9,6 @@ const { checkBody } = require("../modules/checkBody");
 
 router.post("/", (req, res) => {
   User.findOne({ token: req.body.token }).then((data) => {
-    console.log(data);
     if (data) {
       const newTweet = new Tweet({
         content: req.body.content,
@@ -23,13 +22,58 @@ router.post("/", (req, res) => {
   });
 });
 
+router.delete("/", (req, res) => {
+  Tweet.deleteOne({ content: req.body.content }).then((data) => {
+    if (data) {
+      res.json({ result: true, message: "tweet deleted" });
+    } else {
+      res.json({ result: false, error: "tweet not deleted" });
+    }
+  });
+});
+
+router.post("/like", (req, res) => {
+  Tweet.updateOne({ content: req.body.content }, { $inc: { nblike: 1 } }).then(
+    (data) => {
+      if (data) {
+        res.json({ result: true, message: "tweet liked" });
+      } else {
+        res.json({ result: false, error: "User not found" });
+      }
+    }
+  );
+});
+router.post("/unlike", (req, res) => {
+  Tweet.updateOne({ content: req.body.content }, { $inc: { nblike: -1 } }).then(
+    (data) => {
+      if (data) {
+        res.json({ result: true, message: "tweet unliked" });
+      } else {
+        res.json({ result: false, error: "User not found" });
+      }
+    }
+  );
+});
+
 router.get("/alltweet", (req, res) => {
   Tweet.find()
     .populate("user")
     .then((data) => {
-      console.log(data);
       if (data) {
         res.json({ result: true, tweets: data });
+      } else {
+        res.json({ result: false, error: "No tweet found" });
+      }
+    });
+});
+
+router.post("/nblike", (req, res) => {
+  Tweet.findOne({ content: req.body.content })
+    .populate("user")
+    .then((data) => {
+      if (data) {
+        res.json({ result: true, nblike: data.nblike });
+        console.log(data.nblike);
       } else {
         res.json({ result: false, error: "No tweet found" });
       }
